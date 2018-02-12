dc567fde90f541fd4b8d4afd6570f617bd172cb0
Begona <boadicea1979@yahoo.es>
2018-02-12 19:53:30 +0000

    added basic authentication to articles and comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "Begona", password: "jesus", except: [:index, :show]
+
 	def index 
 		@articles = Article.all
 	end
