e48b84041faa48475b35d34dfe436f2972f7ded1
Begona <boadicea1979@yahoo.es>
2018-02-16 16:16:16 +0000

    cleanup since merge of periods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,6 @@
 class ArticlesController < ApplicationController
 	http_basic_authenticate_with name: "Begona", password: "jesus", except: [:index, :show]
 
-	def index 
-		@articles = Article.all
-	end
-
 	def show
 		@article = Article.find(params[:id])
 	end
