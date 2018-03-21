d6c39b4d9d137b046f49f544f235775e719183c0
Begona <boadicea1979@yahoo.es>
2018-03-21 15:32:24 +0000

    set if error exist to show them on period new form

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,6 +1,5 @@
 class Period < ApplicationRecord
 	has_many :articles, inverse_of: :period
 	has_many :characters, inverse_of: :period
-	validates :title, presence: true,
-				      length: {minimum: 5}
+	validates :title, presence: true, length: {minimum: 5}
 end
