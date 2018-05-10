83920fc55697c2ad50cd524c5c35b80705fc0ae5
Begona <boadicea1979@yahoo.es>
2018-05-10 14:06:05 +0100

    Changed the rails h1 element for React Title component in welcome and articles page

--- app/assets/javascripts/components/hello.jsx
+++ app/assets/javascripts/components/hello.jsx
@@ -1,14 +1,34 @@
- class App extends React.Component {
-    render(){
-       return(
-           <div>
-               <h1>Hello </h1>
-           </div>   
-        )
-     }
-}
+class Title extends React.Component {
+  render(){
+   
+    if (this.props.admin_signed_in) {
+    	p = <a href={this.props.path} className="text-muted">{this.props.path_title}</a>;
+    } else {
+    	p = null;
+    }
 
+    return(
+      <div>
+        <h1 className='main'>{this.props.title} {p}</h1>
+      </div>
+    )
+  }
+};
 
+// kjlkj
+// class List extends React.Component {
+// 	render() {
+// 		return(
+// 			<div className='list periods_list'>
+// 				<Link to={period_path(period)}>too</Link>
+// 			</div>
+// 		)
+// 	}
+// }
+// // <Link to={`/users/${user.id}`} activeClassName="active">{user.name}</Link>
 
-
-
+// // <div class="list periods_list">
+// //   <% @periods.each do |period| %>
+// //     <div><%= link_to period.title, period_path(period) %></div>
+// //   <% end %>
+// // </div>
