ff5e9dc010bd31bee35a17c612cde983b92a9d68
Begona <boadicea1979@yahoo.es>
2018-05-11 15:04:43 +0100

    add variable props to index controller

--- app/assets/javascripts/components/hello.jsx
+++ app/assets/javascripts/components/hello.jsx
@@ -15,20 +15,3 @@ class Title extends React.Component {
   }
 };
 
-// kjlkj
-// class List extends React.Component {
-// 	render() {
-// 		return(
-// 			<div className='list periods_list'>
-// 				<Link to={period_path(period)}>too</Link>
-// 			</div>
-// 		)
-// 	}
-// }
-// // <Link to={`/users/${user.id}`} activeClassName="active">{user.name}</Link>
-
-// // <div class="list periods_list">
-// //   <% @periods.each do |period| %>
-// //     <div><%= link_to period.title, period_path(period) %></div>
-// //   <% end %>
-// // </div>
