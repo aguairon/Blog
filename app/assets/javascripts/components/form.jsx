5a5a944326ca2c5a627fc6ded8da2ec3947e40bd
Begona <boadicea1979@yahoo.es>
2018-06-19 15:42:55 +0100

    extrated form logic to another FormContainer

--- app/assets/javascripts/components/form.jsx
+++ app/assets/javascripts/components/form.jsx
@@ -1,66 +1,12 @@
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    let title;
-    let id;
-    if (props.period) {
-      title = props.period.title;
-      id = props.period.id;
-    } else {
-      title = "";
-    }
-
-    this.state = {
-      title: title,
-      valid: true,
-      id: id
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ title: event.target.value });
-    this.setState({valid: true});
-  }
-
-  handleSubmit(event, props) {
-    event.preventDefault();
-    if (this.state.title.length < 5) {
-      this.setState({ valid: false }); 
- 
-    } else {
-      this.setState({ valid: true });
-      const title = this.state.title;
-      let url;
-      let method;
-      if (this.state.id !==  undefined) {
-        url = "/periods/" + this.state.id;
-        method = "PATCH";
-      } else {
-        url = "/periods"
-        method = 'POST'
-      }
-
-      $.ajax({
-        url: url,
-        dataType: 'json',
-        type: method,
-        data:  {period: {title}},
-        success:
-          window.location.href= "/"
-      })
-    }
-  }
 
   render() {
     return(
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.props.onSubmit}>
         <label>
         Título
-          <input type='text' name='title-field' autoFocus  value={this.state.title} onChange={this.handleChange} />
-          {!this.state.valid && <div style={{color:"red"}}>Este título no es válido</div>}
+          <input type='text' name='title-field' autoFocus  value={this.props.value} onChange={this.props.onChange} />
+          {!this.props.valid && <div style={{color:"red"}}>Este título no es válido</div>}
         </label>
         <input type="submit" value="Crear período" />
       </form>
---
+++ app/assets/javascripts/containers/form_container.jsx
@@ -0,0 +1,66 @@
+class FormContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    let title;
+    let id;
+    if (props.period) {
+      title = props.period.title;
+      id = props.period.id;
+    } else {
+      title = "";
+    }
+
+    this.state = {
+      title: title,
+      valid: true,
+      id: id
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ title: event.target.value });
+    this.setState({valid: true});
+  }
+
+  handleSubmit(event, props) {
+    event.preventDefault();
+    if (this.state.title.length < 5) {
+      this.setState({ valid: false }); 
+ 
+    } else {
+      this.setState({ valid: true });
+      const title = this.state.title;
+      let url;
+      let method;
+      if (this.state.id !==  undefined) {
+        url = "/periods/" + this.state.id;
+        method = "PATCH";
+      } else {
+        url = "/periods"
+        method = 'POST'
+      }
+
+      $.ajax({
+        url: url,
+        dataType: 'json',
+        type: method,
+        data:  {period: {title}},
+        success:
+          window.location.href= "/"
+      })
+    }
+  }
+
+  render() {
+    return(
+      <Form 
+        onSubmit={this.handleSubmit}
+        value={this.state.title}
+        onChange={this.handleChange}
+        valid={this.state.valid}/>
+    )
+  }
+}
