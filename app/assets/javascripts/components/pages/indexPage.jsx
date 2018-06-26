73fa3268a8fd6d59f6196b94d5ac007ce238dcb8
Begona <boadicea1979@yahoo.es>
2018-06-26 14:27:24 +0100

    Moved IndexPage to pages folder

--- app/assets/javascripts/components/pages/indexPage.jsx
+++ app/assets/javascripts/components/pages/indexPage.jsx
@@ -2,7 +2,7 @@ class IndexPage extends React.Component{
   render() {
     return(
       <div>
-        <PageTitle {...this.props.title} link/>
+        <PageTitle {...this.props.title} />
         <List {...this.props.list}/>
       </div>
     )
